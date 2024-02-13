b513c8aaa1620a7e141296655ea0190d500aba50
Eri <103050349+Eriarer@users.noreply.github.com>
2024-02-13 16:47:45 -0600

    base project

---
+++ PrimerParcial/11_Tiendida/main.js
@@ -0,0 +1,13 @@
+function chocolate(nombre, precio) {
+  this.nombre = nombre;
+  this.precio = precio;
+}
+
+const chocolates = [
+  new chocolate("Carlos V", 10.99),
+  new chocolate("Ferrero rocher", 15.80),
+  new chocolate("Hershey's blanco", 20.35),
+  new chocolate("KitKat", 12.00),
+  new chocolate("Mr. Beast chocolate", 25.25),
+  new chocolate("Raffaelo", 18.00),
+];
