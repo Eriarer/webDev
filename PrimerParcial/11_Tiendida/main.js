ffbb06334b8610c6aebd4395456c68c3f5808579
Eri <103050349+Eriarer@users.noreply.github.com>
2024-02-13 18:56:20 -0600

    quick changes

--- PrimerParcial/11_Tiendida/main.js
+++ PrimerParcial/11_Tiendida/main.js
@@ -1,13 +1,4 @@
-function chocolate(nombre, precio) {
-  this.nombre = nombre;
-  this.precio = precio;
+const agregarCarrito = (nombre, precio) => {
+  console.log("Agregando al carrito: ",
+    nombre, precio.toFixed(2));
 }
-
-const chocolates = [
-  new chocolate("Carlos V", 10.99),
-  new chocolate("Ferrero rocher", 15.80),
-  new chocolate("Hershey's blanco", 20.35),
-  new chocolate("KitKat", 12.00),
-  new chocolate("Mr. Beast chocolate", 25.25),
-  new chocolate("Raffaelo", 18.00),
-];
