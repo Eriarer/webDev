66a1b8e4adaa354066d7ae5c12396222cf0ae89e
eriarer <eriarer_melgoza+git@hotmail.com>
2024-02-19 13:30:03 -0600

    indentation

--- PrimerParcial/12_Drag&Drop/main.js
+++ PrimerParcial/12_Drag&Drop/main.js
@@ -1,33 +1,33 @@
 let init = () => {
-    let draggables = document.querySelectorAll('.draggable');
-    let dropzones = document.querySelectorAll('.dropzone');
-    let eventType = document.getElementById('eventType');
-    draggables.forEach(draggable => {
-        draggable.addEventListener('dragstart', () => {
-            draggable.classList.add('dragging');
-            eventType.innerHTML = 'dragstart';
-        });
-        draggable.addEventListener('dragend', () => {
-            draggable.classList.remove('dragging');
-            eventType.innerHTML = 'dragend';
-        });
+  let draggables = document.querySelectorAll('.draggable');
+  let dropzones = document.querySelectorAll('.dropzone');
+  let eventType = document.getElementById('eventType');
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+      draggable.classList.add('dragging');
+      eventType.innerHTML = 'dragstart';
     });
-    dropzones.forEach(dropzone => {
-        dropzone.addEventListener('dragover', e => {
-            e.preventDefault();
-        });
-        dropzone.addEventListener('dragenter', () => {
-            eventType.innerHTML = 'dragenter';
-        });
-        dropzone.addEventListener('dragleave', () => {
-            eventType.innerHTML = 'dragleave';
-        });
-        dropzone.addEventListener('drop', e => {
-            e.preventDefault();
-            let draggable = document.querySelector('.draggable.dragging');
-            dropzone.appendChild(draggable);
-        });
+    draggable.addEventListener('dragend', () => {
+      draggable.classList.remove('dragging');
+      eventType.innerHTML = 'dragend';
     });
+  });
+  dropzones.forEach(dropzone => {
+    dropzone.addEventListener('dragover', e => {
+      e.preventDefault();
+    });
+    dropzone.addEventListener('dragenter', () => {
+      eventType.innerHTML = 'dragenter';
+    });
+    dropzone.addEventListener('dragleave', () => {
+      eventType.innerHTML = 'dragleave';
+    });
+    dropzone.addEventListener('drop', e => {
+      e.preventDefault();
+      let draggable = document.querySelector('.draggable.dragging');
+      dropzone.appendChild(draggable);
+    });
+  });
 }
 
 
