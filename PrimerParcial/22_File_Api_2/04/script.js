4228137a193ca684205edf6d50706755327dd0a4
eriarer <eriarer_melgoza+git@hotmail.com>
2024-02-27 09:39:08 -0600

    parte 2 script

--- PrimerParcial/22_File_Api_2/04/script.js
+++ PrimerParcial/22_File_Api_2/04/script.js
@@ -1,1 +1,31 @@
- 
+window.onload = function () {
+  var apifile = (window.File && window.FileReader);
+  if (apifile == false) {
+    alert("APIs no soportadas");
+    return;
+  }
+
+  document.getElementById('soltar').addEventListener('drop', soltarArchivo);
+  document.getElementById('soltar').addEventListener('dragover', moverArchivo);
+}
+
+function soltarArchivo(e) {
+  let archivo = e.dataTransfer.files;
+  e.preventDefault();
+  let total = 0;
+  let info = "";
+  for (let i = 0; i < archivo.length; i++) {
+    info += "<p>"
+    info += "Nombre: " + archivo[i].name + "<br>";
+    info += "Tamano: " + archivo[i].size + "<br>";
+    info += "Tipo: " + archivo[i].type;
+    info += "</p>";
+    total += archivo[i].size;
+  }
+  info += "<p>Totla de KB leídos: " + total / 1024 + " en " + archivo.length + "</p>";
+  document.getElementById("salida").innerHTML = info;
+}
+
+function moverArchivo(e) {
+  e.preventDefault();
+}
