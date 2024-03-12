e29be8de634d37ef7659c0ab45fda8e3279af6b0
eriarer <eriarer_melgoza+git@hotmail.com>
2024-03-12 10:51:51 -0600

    adding my name

--- 34_Angular/src/app/app.component.spec.ts
+++ 34_Angular/src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Abraham Melgoza de la Torre');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 34_Angular');
   });
 });
--- 34_Angular/src/app/app.component.ts
+++ 34_Angular/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = '34_Angular';
+  title = 'Abraham Melgoza de la Torre';
 }
