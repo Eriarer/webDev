7aff15c6e1fb93e15ef37637ef4bfd528b98d755
Eri <103050349+Eriarer@users.noreply.github.com>
2024-03-14 17:56:10 -0600

    Simplifien code

--- test/carouselTest/carousel.js
+++ test/carouselTest/carousel.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const menuButtons = document.querySelectorAll('.slider-menu-button');
   const prevButton = document.querySelector('.slide-prev');
   const nextButton = document.querySelector('.slide-next');
+  const animationSeconds = 0.3;
 
   let currentSlideIndex = 0;
 
@@ -42,62 +43,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Función para cambiar al slide siguiente
   function nextSlide() {
-    hideSlide(currentSlideIndex);
-    currentSlideIndex++;
-    if (currentSlideIndex >= slideItems.length) {
-      currentSlideIndex = 0;
-    }
-    showSlide(currentSlideIndex);
-    updateMenuButton(currentSlideIndex);
-    if (currentSlideIndex === 0) {
+    if (currentSlideIndex === numberOfSlides) {
       // agregar la animacion rc
-      slideItems[0].style.animation = 'slide-rc 0.3s ease-in-out';
-      // agregar la animacion cl
-      slideItems[numberOfSlides].style.animation = 'slide-cl 0.3s ease-in-out';
-      setTimeout(() => {
-        slideItems[0].style.animation = '';
-        slideItems[numberOfSlides].style.animation = '';
-      }, 300);
+      swipeLR(0, currentSlideIndex);
     } else {
       // agregar la animacion rc
-      slideItems[currentSlideIndex].style.animation = 'slide-rc 0.3s ease-in-out';
-      // agregar la animacion cl
-      slideItems[currentSlideIndex - 1].style.animation = 'slide-cl 0.3s ease-in-out';
-      setTimeout(() => {
-        slideItems[currentSlideIndex].style.animation = '';
-        slideItems[currentSlideIndex - 1].style.animation = '';
-      }, 300);
+      swipeLR(currentSlideIndex + 1, currentSlideIndex);
     }
-
   }
 
   // Función para cambiar al slide anterior
   function prevSlide() {
-    hideSlide(currentSlideIndex);
-    currentSlideIndex--;
-    if (currentSlideIndex < 0) {
-      currentSlideIndex = slideItems.length - 1;
-    }
-    showSlide(currentSlideIndex);
-    updateMenuButton(currentSlideIndex);
-    if (currentSlideIndex === numberOfSlides) {
-      // agregar la animacion lc
-      slideItems[0].style.animation = 'slide-cr 0.3s ease-in-out';
-      // agregar la animacion cr
-      slideItems[numberOfSlides].style.animation = 'slide-lc 0.3s ease-in-out';
-      setTimeout(() => {
-        slideItems[0].style.animation = '';
-        slideItems[numberOfSlides].style.animation = '';
-      }, 300);
+    if (currentSlideIndex === 0) {
+      swipeRL(numberOfSlides, currentSlideIndex);
     } else {
-      // agregar la animacion rc
-      slideItems[currentSlideIndex].style.animation = 'slide-lc 0.3s ease-in-out';
-      // agregar la animacion cl
-      slideItems[currentSlideIndex + 1].style.animation = 'slide-cr 0.3s ease-in-out';
-      setTimeout(() => {
-        slideItems[currentSlideIndex].style.animation = '';
-        slideItems[currentSlideIndex + 1].style.animation = '';
-      }, 300);
+      swipeRL(currentSlideIndex - 1, currentSlideIndex);
     }
   }
 
@@ -109,15 +69,62 @@ document.addEventListener('DOMContentLoaded', function () {
   menuButtons.forEach((button, index) => {
     button.addEventListener('click', function (event) {
       event.preventDefault(); // Evita el comportamiento predeterminado del enlace
-      currentSlideIndex = index;
-      showSlide(currentSlideIndex);
-      updateMenuButton(currentSlideIndex);
+      // mover de izquierda a derecha
+      if (currentSlideIndex === numberOfSlides && index === 0) {
+        // agregar la animacion rc
+        swipeLR(index, currentSlideIndex);
+      } else if (currentSlideIndex === 0 && index === numberOfSlides) {
+        // mover de derecha a izquierda
+        // agregar la animacion lc
+        swipeRL(index, currentSlideIndex);
+      } else if (currentSlideIndex < index) {
+        // mover de izquierda a derecha
+        // agregar la animacion rc
+        swipeLR(index, currentSlideIndex);
+      } else {
+        // mover de derecha a izquierda
+        // agregar la animacion lc
+        swipeRL(index, currentSlideIndex);
+      }
     });
   });
 
+
+  function swipeLR(itemA, itemB) {
+    hideSlide(currentSlideIndex);
+    showSlide(itemA);
+    updateMenuButton(itemA);
+    currentSlideIndex = itemA;
+    // agregar la animacion rc
+    slideItems[itemA].style.animation = `slide-rc ${animationSeconds}s ease-in-out`;
+    // agregar la animacion cl
+    slideItems[itemB].style.animation = `slide-cl ${animationSeconds}s ease-in-out`;
+    setTimeout(() => {
+      slideItems[itemA].style.animation = '';
+      slideItems[itemB].style.animation = '';
+    }, animationSeconds * 1000);
+  }
+
+  function swipeRL(indexA, indexB) {
+    hideSlide(currentSlideIndex);
+    showSlide(indexA);
+    updateMenuButton(indexA);
+    currentSlideIndex = indexA;
+    // agregar la animacion lc
+    slideItems[indexA].style.animation = `slide-lc ${animationSeconds}s ease-in-out`;
+    // agregar la animacion cr
+    slideItems[indexB].style.animation = `slide-cr ${animationSeconds}s ease-in-out`;
+    setTimeout(() => {
+      slideItems[indexA].style.animation = '';
+      slideItems[indexB].style.animation = '';
+    }, animationSeconds * 1000);
+    updateMenuButton(currentSlideIndex);
+  }
+
+
   // Mostrar el primer slide al cargar la página
   showSlide(currentSlideIndex);
-
+  updateMenuButton(currentSlideIndex);
   // Desplazamiento automático cada 3 segundos
   //setInterval(autoScroll, 3000);
 
