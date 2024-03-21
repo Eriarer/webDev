415f85eedb25553732665e005127cbfb77d7f071
eriarer <eriarer_melgoza+git@hotmail.com>
2024-03-21 11:59:50 -0600

    doble binding

--- 42_DobleBinding/src/app/app.component.ts
+++ 42_DobleBinding/src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '42_DobleBinding';
+  n1!: number;
+  n2!: number;
+  resultado!: number;
+  op: string = '';
+
+  ejecutar(): void {
+    this.op = this.op.trim();
+    switch (this.op) {
+      case '+': this.resultado = this.n1 + this.n2; break;
+      case '-': this.resultado = this.n1 - this.n2; break;
+      case '*': this.resultado = this.n1 * this.n2; break;
+      default:
+        this.resultado = 0;
+        alert('Error enn el operador');
+    }
+  }
+
+  limpiar(): void {
+    this.n1 = 0;
+    this.n2 = 0;
+    this.op = '';
+    this.resultado = 0;
+  }
 }
