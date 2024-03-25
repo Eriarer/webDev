1a6d75149653bb8c15f8b9e9706d02f54021b6c1
eriarer <eriarer_melgoza+git@hotmail.com>
2024-03-25 08:25:41 -0600

    Directivas

---
+++ 46_directivas/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = '44_directivas';
+  datos: personas[] = [
+    { id: 1, nombre: 'Eri', apellido: 'Melgoza', sexo: 'F' },
+    { id: 2, nombre: 'Abraham', apellido: 'Melgoza', sexo: 'M' },
+    { id: 3, nombre: 'Luis', apellido: 'Nijares', sexo: 'M' },
+    { id: 4, nombre: 'Cecilia', apellido: 'Himelda', sexo: 'F' }
+  ];
+}
+
+interface personas {
+  id: number;
+  nombre: string;
+  apellido: string;
+  sexo: string;
+}
