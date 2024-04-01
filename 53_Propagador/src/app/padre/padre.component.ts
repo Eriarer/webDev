07a1c61e5afc0d44a114dc8104a5ae638c3f5ff1
Eri <103050349+Eriarer@users.noreply.github.com>
2024-04-01 16:23:05 -0600

    padre component

--- 53_Propagador/src/app/padre/padre.component.ts
+++ 53_Propagador/src/app/padre/padre.component.ts
@@ -10,12 +10,13 @@ import { HijoComponent } from '../hijo/hijo.component';
 })
 export class PadreComponent {
   title: string = "Vota por tu color favorito";
-  votacion: string = "";
+  votacion: string = "Esperando votos...<br>Porfavor, selecciona un color.<br>Gracias!";
   c1: number = 0;
   c2: number = 0;
   c3: number = 0;
   c4: number = 0;
   totalVotos: number = 0;
+  finalizado: boolean = false;
 
   votos = [
     { title: "Rojo", boton: "btn-danger", id: 1 },
@@ -24,24 +25,26 @@ export class PadreComponent {
     { title: "Amarillo", boton: "btn-warning", id: 4 }
   ];
 
-  finalizar(): void {
-    this.totalVotos = this.c1 + this.c2 + this.c3 + this.c4;
-  }
-
   addVoto(response: number) {
+    this.votacion = "Gracias por decirnos<br>que tu color favorito es el<br>"
     switch (response) {
       case 1:
         this.c1++;
+        this.votacion += "<span class='text-danger'>rojo!</span>";
         break;
       case 2:
         this.c2++;
+        this.votacion += "<span class='text-primary'>azul!</span>";
         break;
       case 3:
         this.c3++;
+        this.votacion += "<span class='text-success'>verde!</span>";
         break;
       case 4:
         this.c4++;
+        this.votacion += "<span class='text-warning'>amarillo!</span>";
         break;
     }
+    this.totalVotos++;
   }
 }
