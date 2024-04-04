d917e0652087e3a937238b36ec9099025caedc1c
eriarer <eriarer_messiah+git@hotmail.com>
2024-04-04 16:15:51 -0600

    Base proyect of services (service array prepared)

---
+++ 57_Servicios/src/app/components/mostrar/mostrar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar',
+  standalone: true,
+  imports: [],
+  templateUrl: './mostrar.component.html',
+  styleUrl: './mostrar.component.css'
+})
+export class MostrarComponent {
+
+}
