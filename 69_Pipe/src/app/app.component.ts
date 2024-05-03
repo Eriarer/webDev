db33a4f0a14b1fae948d59fb93a14b575319c461
eriarer <eriarer_melgoza+git@hotmail.com>
2024-05-03 08:47:21 -0600

    pipe personalzado

--- 69_Pipe/src/app/app.component.ts
+++ 69_Pipe/src/app/app.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CapitalizadoPipe } from './capitalizado.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, CapitalizadoPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = '69_Pipe';
+  clientes: string[] = [
+    'MarIaNA aViLA RIveRA',
+    'GerARDo DiOSdado ESCALERa',
+    'LeoANARDO GuTIeRRez FloRes',
+    'AbRAhaM MeLGOzA dE la TorRe'
+  ]
 }
