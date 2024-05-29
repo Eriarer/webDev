c1916db2b39350b665b8a3e30d998cd896ca8db8
eriarer <eriarer_melgoza+git@hotmail.com>
2024-05-29 06:24:03 -0600

    NodeJs MySql

---
+++ 88_NodeMySql/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connection = require('./conexion');
+const app = express();
+
+const rutas = require('./routes/rutas');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', rutas);
+
+connection.connect((err, res) => {
+    if (err) {
+        console.log(err);
+        console.log('Error al conectar a la base de datos');
+        return;
+    }
+    console.log('Conectado a la base de datos');
+});
+
+app.listen(port, (err, res) => {
+    if (err) {
+        console.log(`Error al iniciar el servidor: ${err}`);
+    } else {
+        console.log(`Servidor iniciado en: http://localhost:${port}`);
+    }
+});
+
+
